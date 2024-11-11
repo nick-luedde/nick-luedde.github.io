09d0da01dbea743c7b57931dfa3c4bebbe6aa1cf
nick <nluedde@outlook.com>
2024-11-11 07:56:40 -0700

    /dist -> /docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    outDir: 'docs',
     // don't minify to leave code as written in the build output
     minify: false
   }
